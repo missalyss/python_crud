740db77712f89a10c1ec5aa14aa5f955ef02eaf7
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-05-21 17:50:49 -0700

    show one route working. working on method overriding put and delete

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,36 @@
 from flask import render_template, request, url_for, redirect
 from app import app, models, db
 from flask_sqlalchemy import SQLAlchemy
+from app import method_override
 
 @app.route('/')
 def index():
     sandwiches = models.Sandwich.query.all()
     return render_template('index.html', sandwiches=sandwiches)
 
-@app.route('/index/')
-def maybe():
-    return "does this work too?"
+@app.route('/edit_form/<id>')
+def edit_form(id):
+    sandwich = models.Sandwich.query.filter_by(id=id).first()
+    return render_template('edit_form.html', sandwich=sandwich)
+
+@app.route('/edit/<id>')
+def put(id):
+    sandwich = models.Sandwich(request.form['name'], request.form['isSandwich'])
+    db.session.add(sandwich)
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/delete/<id>', methods=['DELETE'])
+def destroy(id):
+    sandwich = models.Sandwich('name', 'isSandwich')
+    db.session.delete(sandwich)
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/<id>')
+def show(id):
+    sandwich = models.Sandwich.query.filter_by(id=id).first()
+    return render_template('show.html', sandwich=sandwich)
 
 @app.route('/new_sandwich', methods=['POST'])
 def new_sandwich():
